2214ef02fca4d575e63bce3c7565fabfe8a517de
Mindy Miller <mmillerks@gmail.com>
2022-02-15 21:13:31 -0600

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -18,5 +18,17 @@ app.get('/greeting", (req, res) => {
 app.post('/:name', (req, res) => {
     res.send("What's up, ${name}");
 });
+//Pretty sure I don't have this right. Might need to be req.params.name??
+//sbould it be /users/:name???
 
+
+//TIP CALCULATOR
+app.get('/tip/:total/:tipPercentage', function (req,res) {
+    res.send(req.params);
+});
+//Should I be modifying the req.params like req.params.tipPercentage???
+//I think I need to do this to get the value of the tip? 
+
+
+//
 
